5da93dce358c640e3f2ef275d72991025e562e01
3on <jr@3on.fr>
2011-10-13 05:01:40 +0200

    Initial Commit

---
+++ Canvas Whiteboard/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Canvas Whiteboard
+//
+//  Created by Jr on 13/10/11.
+//  Copyright 3on 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
